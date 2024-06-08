d09b4248b75c6aaacb589bbee10f1d08a39de6c4
Arat Rana <arana2@opentext.com>
2024-06-08 23:13:31 +0530

    Update config.js

--- movieFanSite/config.js
+++ movieFanSite/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  clientID: "Iv23liWWjn8GT5FR4jDD",
-  clientSecret: "d5c6cf8aa8734b14d25a588ce8d79302f8c578b8",
+  clientID: "",
+  clientSecret: "",
   callbackURL: "http://localhost:3000/auth",
 };
